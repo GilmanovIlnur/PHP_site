2a4ddf4af8e2b8251544c5dfeb776efd8227b58c
GilmanovIlnur <qazxsw777>
2019-10-21 23:30:40 +0300

    Добавил главную страницу сайта

---
+++ MainPageContent.php
@@ -0,0 +1,24 @@
+<?php
+
+class MainPageContent implements IContent
+{
+
+    function get_title1()
+    {
+        return "";
+    }
+
+    function get_title2()
+    {
+        return "";
+    }
+
+    function show_content()
+    {
+        print <<<HEADER
+        <div style="text-align: center; padding-top: 200px">
+            <h3 style="color: #ff7c2b">Сайт, предназначенный для курса по PHP. </br> Можно найти кое-что полезное.</h3>
+        </div>
+HEADER;
+    }
+}
